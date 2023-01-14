7dd3fada564e345afe9dcd61d2fe97d841cae3d7
citytexi <43637679+citytexi@users.noreply.github.com>
2023-01-14 12:52:21 +0900

    [PR] #2 이후 PR 오류 수정 (#3)
        [Add] feature/splash_screen [23/01/10] (#1)
    
    * [Modify] PR 오류 수정

--- presentation/src/main/java/com/mediaproject/presentation/common/nav/Navgraph.kt
+++ presentation/src/main/java/com/mediaproject/presentation/common/nav/Navgraph.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.mediaproject.presentation.common.route.Screen
+import com.mediaproject.presentation.common.screen.route.Screen
 import com.mediaproject.presentation.common.screen.LandingScreen
 import com.mediaproject.presentation.screen.signin.SignInScreen
 import com.mediaproject.presentation.screen.signup.SignUpScreen
